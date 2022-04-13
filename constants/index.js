361ad55dd1d2d0f4b97f3bda233d6d9d79e4d4f0
OleanjiKingCode <holamilean56@gmail.com>
2022-04-13 23:41:11 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x5f6ACccc002b388d5a524a6c703121320b5Ae05D";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
